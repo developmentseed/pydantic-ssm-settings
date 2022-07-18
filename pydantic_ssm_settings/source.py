44d0f94d729604bd4018dd96ac32d7da26c3e507
Anthony Lukach <anthonylukach@gmail.com>
2022-07-18 15:52:24 -0600

    Fixes

--- pydantic_ssm_settings/source.py
+++ pydantic_ssm_settings/source.py
@@ -7,7 +7,10 @@ from botocore.exceptions import ClientError
 from botocore.client import Config
 import boto3
 
-from pydantic import BaseSettings, typing, fields
+from pydantic import BaseSettings
+from pydantic.typing import StrPath, get_origin, is_union
+from pydantic.utils import deep_update
+from pydantic.fields import ModelField
 
 if TYPE_CHECKING:
     from mypy_boto3_ssm.client import SSMClient
@@ -23,8 +26,13 @@ class SettingsError(ValueError):
 class AwsSsmSettingsSource:
     __slots__ = ("ssm_prefix",)
 
-    def __init__(self, ssm_prefix: Optional[typing.StrPath]):
-        self.ssm_prefix: Optional[typing.StrPath] = ssm_prefix
+    def __init__(
+        self,
+        ssm_prefix: Optional[StrPath],
+        env_nested_delimiter: Optional[str] = None,
+    ):
+        self.ssm_prefix: Optional[StrPath] = ssm_prefix
+        self.env_nested_delimiter: Optional[str] = env_nested_delimiter
 
     @property
     def client(self) -> "SSMClient":
@@ -73,7 +81,7 @@ class AwsSsmSettingsSource:
             case_sensitive=settings.__config__.case_sensitive,
         )
 
-        # The following was lifted directly from https://github.com/samuelcolvin/pydantic/blob/a21f0763ee877f0c86f254a5d60f70b1002faa68/pydantic/env_settings.py#L165-L237
+        # The following was lifted directly from https://github.com/samuelcolvin/pydantic/blob/a21f0763ee877f0c86f254a5d60f70b1002faa68/pydantic/env_settings.py#L165-L237  # noqa
         for field in settings.__fields__.values():
             env_val: Optional[str] = None
             for env_name in field.field_info.extra["env_names"]:
@@ -85,11 +93,12 @@ class AwsSsmSettingsSource:
             if is_complex:
                 if env_val is None:
                     # field is complex but no value found so far, try explode_env_vars
-                    env_val_built = self.explode_env_vars(field, ssm_values)
+                    env_val_built = self.explode_ssm_values(field, ssm_values)
                     if env_val_built:
                         d[field.alias] = env_val_built
                 else:
-                    # field is complex and there's a value, decode that as JSON, then add explode_env_vars
+                    # field is complex and there's a value, decode that as JSON, then
+                    # add explode_env_vars
                     try:
                         env_val = settings.__config__.json_loads(env_val)
                     except ValueError as e:
@@ -100,7 +109,7 @@ class AwsSsmSettingsSource:
 
                     if isinstance(env_val, dict):
                         d[field.alias] = deep_update(
-                            env_val, self.explode_env_vars(field, ssm_values)
+                            env_val, self.explode_ssm_values(field, ssm_values)
                         )
                     else:
                         d[field.alias] = env_val
@@ -110,14 +119,14 @@ class AwsSsmSettingsSource:
 
         return d
 
-    def field_is_complex(self, field: fields.ModelField) -> Tuple[bool, bool]:
+    def field_is_complex(self, field: ModelField) -> Tuple[bool, bool]:
         """
         Find out if a field is complex, and if so whether JSON errors should be ignored
         """
         if field.is_complex():
             allow_json_failure = False
         elif (
-            typing.is_union(typing.get_origin(field.type_))
+            is_union(get_origin(field.type_))
             and field.sub_fields
             and any(f.is_complex() for f in field.sub_fields)
         ):
@@ -127,8 +136,8 @@ class AwsSsmSettingsSource:
 
         return True, allow_json_failure
 
-    def explode_env_vars(
-        self, field: fields.ModelField, env_vars: Mapping[str, Optional[str]]
+    def explode_ssm_values(
+        self, field: ModelField, env_vars: Mapping[str, Optional[str]]
     ) -> Dict[str, Any]:
         """
         Process env_vars and extract the values of keys containing env_nested_delimiter into nested dictionaries.
